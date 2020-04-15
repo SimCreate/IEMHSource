fb911e1bcb72f1c8e4b284166774e9c9fd696eda
SimCreate <wdg1057@gmail.com>
2020-04-15 21:21:41 +0900

    DB에서 데이터를 끌고 오도록 소스 수정

---
+++ SearchMoudle/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace SearchMoudle
+{
+    partial class SearchForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SearchCbo = new System.Windows.Forms.ComboBox();
+            this.BtnExcel = new System.Windows.Forms.Button();
+            this.BrnPrt = new System.Windows.Forms.Button();
+            this.BtnRefresh = new System.Windows.Forms.Button();
+            this.SearchGrid = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.SearchGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.SearchCbo);
+            this.panel1.Controls.Add(this.BtnExcel);
+            this.panel1.Controls.Add(this.BrnPrt);
+            this.panel1.Controls.Add(this.BtnRefresh);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(506, 63);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 19);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "조회조건";
+            // 
+            // SearchCbo
+            // 
+            this.SearchCbo.FormattingEnabled = true;
+            this.SearchCbo.Location = new System.Drawing.Point(82, 20);
+            this.SearchCbo.Name = "SearchCbo";
+            this.SearchCbo.Size = new System.Drawing.Size(121, 25);
+            this.SearchCbo.TabIndex = 3;
+            // 
+            // BtnExcel
+            // 
+            this.BtnExcel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnExcel.Location = new System.Drawing.Point(418, 6);
+            this.BtnExcel.Name = "BtnExcel";
+            this.BtnExcel.Size = new System.Drawing.Size(81, 50);
+            this.BtnExcel.TabIndex = 2;
+            this.BtnExcel.Text = "Excel";
+            this.BtnExcel.UseVisualStyleBackColor = true;
+            // 
+            // BrnPrt
+            // 
+            this.BrnPrt.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.BrnPrt.Location = new System.Drawing.Point(331, 6);
+            this.BrnPrt.Name = "BrnPrt";
+            this.BrnPrt.Size = new System.Drawing.Size(81, 50);
+            this.BrnPrt.TabIndex = 1;
+            this.BrnPrt.Text = "출력";
+            this.BrnPrt.UseVisualStyleBackColor = true;
+            // 
+            // BtnRefresh
+            // 
+            this.BtnRefresh.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnRefresh.Location = new System.Drawing.Point(244, 6);
+            this.BtnRefresh.Name = "BtnRefresh";
+            this.BtnRefresh.Size = new System.Drawing.Size(81, 50);
+            this.BtnRefresh.TabIndex = 0;
+            this.BtnRefresh.Text = "조회";
+            this.BtnRefresh.UseVisualStyleBackColor = true;
+            this.BtnRefresh.Click += new System.EventHandler(this.BtnRefresh_Click);
+            // 
+            // SearchGrid
+            // 
+            this.SearchGrid.BackgroundColor = System.Drawing.Color.White;
+            this.SearchGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.SearchGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.SearchGrid.GridColor = System.Drawing.Color.White;
+            this.SearchGrid.Location = new System.Drawing.Point(0, 63);
+            this.SearchGrid.Name = "SearchGrid";
+            this.SearchGrid.RowTemplate.Height = 23;
+            this.SearchGrid.Size = new System.Drawing.Size(506, 473);
+            this.SearchGrid.TabIndex = 1;
+            // 
+            // SearchForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(506, 536);
+            this.Controls.Add(this.SearchGrid);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("맑은 고딕", 10F);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "SearchForm";
+            this.Text = "SearchForm";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.SearchGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox SearchCbo;
+        private System.Windows.Forms.Button BtnExcel;
+        private System.Windows.Forms.Button BrnPrt;
+        private System.Windows.Forms.Button BtnRefresh;
+        private System.Windows.Forms.DataGridView SearchGrid;
+    }
+}
+
---
+++ SearchMoudle/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace SearchMoudle
+{
+    public partial class SearchForm : Form
+    {
+        DBClass dbClas = new DBClass();
+
+        public SearchForm()
+        {
+            InitializeComponent();
+            string pwd = string.Empty;      // DB 비밀번호
+            dbClas.SetConInfo("127.0.0.1", "mysql", "root", pwd);
+            InitComboBox();
+            SearchGrid.AutoGenerateColumns = true;
+        }
+
+        private void InitComboBox()
+        {
+            DataSet ds;
+
+            string query = @"SELECT CLASS FROM PRODUCTINFOS";
+
+            if (dbClas.ExecureReader(out ds, query, null) > 0)
+            {
+                DataTable dT = ds.Tables[0];
+
+                foreach (DataRow dRow in dT.Rows)
+                {
+                    SearchCbo.Items.Add(dRow["CLASS"].ToString());
+                }
+            }
+        }
+
+        private void BtnRefresh_Click(object sender, EventArgs e)
+        {
+            SearchGrid.DataSource = null;
+
+            DataSet ds;
+            string query = @"SELECT * FROM PRODUCTINFOS
+                            WHERE CLASS = @";
+
+            List<object> pList = new List<object>();
+            pList.Add(SearchCbo.Text);
+
+            BindingSource source = new BindingSource();
+
+            if (dbClas.ExecureReader(out ds, query, pList.ToArray()) > 0)
+            {
+                source.DataSource = ds.Tables[0];
+                SearchGrid.DataSource = source;
+                //SearchGrid.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.Fill);
+                SearchGrid.Refresh();
+            }
+        }
+
+    }
+
+    public class DBClass
+    {
+        string strConInfo = string.Empty;
+
+        public void SetConInfo(string pServer, string pDBName, string pID, string pPwd)
+        {
+            StringBuilder strConn = new StringBuilder();
+            strConn.AppendFormat("Server = {0}; Database = {1}; Uid = {2}; Pwd = {3};", pServer, pDBName, pID, pPwd);
+            //"Server = ?; Database = ?; Uid = ?; Pwd=?;";
+            strConInfo = strConn.ToString();
+        }
+
+        public int ExecuteNonQuery(string pQuery, params Object[] pParameter)
+        {
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(strConInfo))
+                {
+                    MySqlCommand cmd = conn.CreateCommand();
+                    conn.Open();
+                    cmd.CommandText = ConvertString(pQuery, pParameter);
+                    int ret = cmd.ExecuteNonQuery();
+                    conn.Close();
+                    return ret;
+                }             
+            }
+            catch (Exception ex)
+            {
+                return -1;
+            }
+        }
+
+        public int ExecureReader(out DataSet ds, string query, params Object[] pParams)
+        {
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(strConInfo))
+                {
+                    ds = new DataSet();
+                    conn.Open();
+
+                    MySqlDataAdapter adpt = new MySqlDataAdapter(ConvertString(query, pParams), conn);
+                    adpt.Fill(ds);
+                    conn.Close();
+                    return 1;
+                }
+            }
+            catch (Exception ex)
+            {
+                ds = null;
+                return -1;
+            }
+        }
+
+        public string ConvertString(string pQuery, params Object[] pParams)
+        {
+            int idx = 0;
+            string newString = string.Empty;
+
+            foreach (char c in pQuery)
+            {
+                if (c == '@')
+                {
+                    var param = pParams[idx];
+
+                    if (param is int)
+                    {
+                        newString += Convert.ToInt32(param);
+                    }
+
+                    if (param is string)
+                    {
+                        newString += "'" + param.ToString() + "'";
+                    }
+                }
+                else
+                {
+                    newString += c;
+                }
+            }
+
+            return newString;
+        }
+
+
+    }
+
+
+}
